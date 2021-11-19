cf98b481e66bac82e22af9224d6b3a4fb1470cf0
vukhoa20122003@gmail.com <vukhoa20122003@gmali.com>
2021-11-20 00:46:35 +0700

    day code

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hello ! 🖐🖐🖐',
+    introDesc: `Bạn hãy trả lời thật lòng nha😘`,
+    btnIntro: 'OK',
+    title: 'Bạn thật đáng yêu,Bạn thích tôi phải không? 😙',
+    desc: 'Bạn không trả lời hoặc thoát ra, tức là Bạn thích Mình đó nha :> ',
+    btnYes: 'Có, Mình thật sự thích Bạn 💖💖💖',
+    btnNo: '💔 Bạn mơ à ! 💔 ',
+    question:'Nói cho MÌnh biết đi vì sao BẠN thích mình 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì Bạn đẹp trai đó Ahiiiii 😊😊',
+    mess: 'TỚ biết mà,Tớ cũng thích bạn lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Tớ nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/100009637576567'
+}
